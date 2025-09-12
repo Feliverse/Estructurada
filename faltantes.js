4c0090d788151ca063779e64217bccbb8ec824df
Felipe Haybar Valenzuela <114632980+Feliverse@users.noreply.github.com>
2025-09-12 19:21:55 -0400

    Ejercicio 1

---
+++ faltantes.js
@@ -0,0 +1,5 @@
+//Dado un array de enteros ordenado y sin repetidos, crea una función que calcule
+//* y retorne todos los que faltan entre el mayor y el menor.
+
+let arr = [1, 3, 5]; // debe retornar 2 4
+let arr1 = [-2 ,2, 6]; // debe retornar -1 0 1 3 4 5
