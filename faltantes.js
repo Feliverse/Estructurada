fe94e3e15b6643f5933302d888843d68cf30bea7
Felipe Haybar Valenzuela <114632980+Feliverse@users.noreply.github.com>
2025-09-16 19:50:33 -0400

    Fizz Buz con Ternarios

--- faltantes.js
+++ faltantes.js
@@ -1,5 +1,32 @@
-//Dado un array de enteros ordenado y sin repetidos, crea una función que calcule
-//* y retorne todos los que faltan entre el mayor y el menor.
+// Dado un array de enteros ordenado y sin repetidos, 
+// crea una función que calcule
+// y retorne todos los que faltan entre el mayor y el menor.
 
-let arr = [1, 3, 5]; // debe retornar 2 4
+/* let arr = [1, 3, 5]; // debe retornar 2 4
 let arr1 = [-2 ,2, 6]; // debe retornar -1 0 1 3 4 5
+*/
+
+function numerosFaltantes(arr) {
+    let faltantes = [];
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+
+    let rango = []
+    for (let i = min; i <= max; i++) {
+        rango.push(i);
+    }
+
+    for (let num of rango) {
+        if (!arr.includes(num)) {
+            faltantes.push(num);
+        }
+    }
+    
+    return faltantes;
+}
+
+let arr = [1, 3, 5];
+console.log(numerosFaltantes(arr));
+
+let arr1 = [-2, 2, 6];
+console.log(numerosFaltantes(arr1));
